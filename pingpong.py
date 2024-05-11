71149b6265f1ed9955ba772f22a915e54c0c7e73
TAybars06 <atalay-burhan@hotmail.com>
2024-05-11 21:13:21 +0300

    İki farklı oyuncu için tuş ataması yapıldı.

--- pingpong.py
+++ pingpong.py
@@ -14,13 +14,18 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
 back = (200, 255, 255) #arka plan rengi (background)
 win_width = 600
 win_height = 500
